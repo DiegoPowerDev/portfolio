b4cb414851b544878b960f9feabcc1088c742df2
Diego Torres <diego_torres_11@hotmail.com>
2026-02-21 10:58:12 -0500

    añadiendo sitemaps y robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://diegotorres-portfoliodev.vercel.app",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  robotsTxtOptions: {
+    additionalSitemaps: [],
+  },
+};
